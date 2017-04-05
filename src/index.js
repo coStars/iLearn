8197e76218b58921896dd2e86b42ac06be82f248
ibraghada@gmail.com <ibraghada@gmail.com>
2017-04-05 05:07:11 -0700

    make function stateless

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import {render} from 'react-dom'
-import LearnList from './LearnList.js'
-const Main = ()=>(
-  <div>
-  <h1>ILearn</h1>
-  <LearnList  />
-</div>
-)
+// import LearnTable from './LearnTable.js'
+// import LearnForm from './LearnForm.js'
+class Main extends React.Component {
+  render() {
+    return (
+      // <div>
+      <h1>alaaa</h1>
+      // <LearnForm/>
+      // <LearnTable/>
+      // </div>
+    )
+  }
+}
 render(
-  <Main/>,
+  <h1>ok</h1>,
   document.getElementById('hello-world')
 );
