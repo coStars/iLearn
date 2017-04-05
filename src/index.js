8b21d7774d92b62b03adf9348237f0414c0b058a
4daliaomry4 <alaakhattab92@gmail.com>
2017-04-05 11:25:15 +0300

    solving issue

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import React from 'react'
 import {render} from 'react-dom'
-import BookList from './BookList.js'
-
-const BookContentPage = (props) => (
-    <div>
-    <h1>ILearn</h1>
-    <BookList bookList={props.bookList} />
-  </div>
+import LearnList from './LearnList.js'
+const Main = ()=>(
+  <div>
+  <h1>ILearn</h1>
+  <LearnList  />
+</div>
 )
-class Main extends React.Component {
-  render () {
-    const bookList = [{
-      tech: 'elm',
-      docs: <a href="http://www.elm-lang.org/">www.elm-lang.org</a>
-    }]
-
-    return (
-      <BookContentPage
-        bookList={bookList}
-      />
-    )
-  }
-}
 render(
   <Main/>,
   document.getElementById('hello-world')
-)
+);
