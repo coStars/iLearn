3bfbe38dbdc0580244e5e94b0f9e7baac1d5cbc6
ibraghada@gmail.com <ibraghada@gmail.com>
2017-04-05 07:03:28 -0700

    edit

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import {render} from 'react-dom'
  import LearnForm from './LearnForm.js'
 
 class Main extends React.Component {
+
   render() {
     return (
        <div>
-         <h1>alaaa - it works!</h1>
-         <LearnForm/>
-         <LearnTable/>
+         <h1>ILearn</h1>
+         <LearnForm />
+         <LearnTable />
        </div>
     )
   }
